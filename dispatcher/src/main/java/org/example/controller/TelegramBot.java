9423c972c24387af77ffd1a8e0e3c9b528e9ebde
NikitaUtk <nikita.utkn@gmail.com>
2022-10-17 16:13:15 +0400

    add RabbitMQ

--- dispatcher/src/main/java/org/example/controller/TelegramBot.java
+++ dispatcher/src/main/java/org/example/controller/TelegramBot.java
@@ -1,14 +1,14 @@
 package org.example.controller;
 
 import lombok.extern.log4j.Log4j;
-import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
-import lombok.var;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import javax.annotation.PostConstruct;
 
 
 @Component
@@ -19,6 +19,18 @@ public class TelegramBot extends TelegramLongPollingBot {
     private String botName;
     @Value("${bot.token}")
     private String botToken;
+
+    private UpdateController updateController;
+    public TelegramBot(UpdateController updateController) {
+        this.updateController = updateController;
+    }
+    @PostConstruct
+    public void init(){
+        updateController.registerBot(this);
+    }
+
+
+
 
     //private static final Logger log = Logger.getLogger(TelegramBot.class);
     @Override
@@ -33,13 +45,13 @@ public class TelegramBot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-        var originalMessage = update.getMessage();
-        log.debug(originalMessage.getText());
-
-        var responce = new SendMessage();
+        /*var originalMessage = update.getMessage();
+        log.debug(originalMessage.getText());*/
+        updateController.processUpdate(update);
+        /*var responce = new SendMessage();
         responce.setChatId(originalMessage.getChatId().toString());
         responce.setText("Hello from bot");
-        sendAnswerMessage(responce);
+        sendAnswerMessage(responce);*/
     }
 
     public void sendAnswerMessage(SendMessage message){
---
+++ dispatcher/src/main/java/org/example/controller/UpdateController.java
@@ -0,0 +1,79 @@
+package org.example.controller;
+
+import lombok.extern.log4j.Log4j;
+import lombok.var;
+import org.example.service.UpdateProducer;
+import org.example.utils.MessageUtils;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+import static org.example.model.RabbitQueue.*;
+
+@Component
+@Log4j
+public class UpdateController {
+    private TelegramBot telegramBot;
+    private final MessageUtils messageUtils;
+    private final UpdateProducer updateProducer;
+
+    public UpdateController(MessageUtils messageUtils, UpdateProducer updateProducer) {
+        this.messageUtils = messageUtils;
+        this.updateProducer = updateProducer;
+    }
+
+    public void registerBot(TelegramBot telegramBot){
+        this.telegramBot = telegramBot;
+    }
+
+    public void processUpdate(Update update){
+        if(update == null){
+            log.error("Received update is null");
+            return;
+        }
+        if(update.getMessage() != null){
+            DistributeMessageByType(update);
+        }else {
+            log.error("Unsupported message type is received: " + update);
+        }
+    }
+
+    private void DistributeMessageByType(Update update) {
+        var message = update.getMessage();
+        if(message.getText() != null){
+            ProcessTextMessage(update);
+        }else if (message.getDocument() != null){
+            ProcessDocMessage(update);
+        }else if (message.getPhoto() != null){
+            ProcessPhotoMessage(update);
+        }else{
+            setUnsupportetMessageTypeView(update);
+        }
+    }
+
+    private void setUnsupportetMessageTypeView(Update update) {
+        var sendMessage = messageUtils.generateSendMessageWithText(update, "Unsupported message type!");
+        setView(sendMessage);
+    }
+    private void setFileIsReceivedView(Update update) {
+        var sendMessage = messageUtils.generateSendMessageWithText(update, "File received, is processed...");
+        setView(sendMessage);
+    }
+    private void setView(SendMessage sendMessage) {
+        telegramBot.sendAnswerMessage(sendMessage);
+    }
+
+    private void ProcessPhotoMessage(Update update) {
+        updateProducer.producer(PHOTO_MESSAGE_UPDATE, update);
+        setFileIsReceivedView(update);
+    }
+
+    private void ProcessDocMessage(Update update) {
+        updateProducer.producer(DOC_MESSAGE_UPDATE, update);
+        setFileIsReceivedView(update);
+    }
+
+    private void ProcessTextMessage(Update update) {
+        updateProducer.producer(TEXT_MESSAGE_UPDATE, update);
+    }
+}
